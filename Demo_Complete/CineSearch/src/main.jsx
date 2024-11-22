43019d79eb94474e6e01600a7070eea1a6c1b016
DeepBiswas <105808324+DeepBwas@users.noreply.github.com>
2024-11-22 00:54:55 -0500

    New components!

--- Demo_Complete/CineSearch/src/main.jsx
+++ Demo_Complete/CineSearch/src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header.jsx'
+import MovieCard from './MovieCard.jsx';
+//import MoviesContainer from './MoviesContainer.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
+    <MovieCard />
   </StrictMode>,
 )
